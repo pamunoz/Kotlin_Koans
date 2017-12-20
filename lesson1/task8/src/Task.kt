2b0cf1ebb6e0666316e0b0f5e97cd5a5fbd43a05
Pablo Munoz <pfariasmunoz@gmail.com>
2017-12-20 15:22:42 -0300

    Add Nullable Types

--- lesson1/task8/src/Task.kt
+++ lesson1/task8/src/Task.kt
@@ -1,11 +1,15 @@
 fun sendMessageToClient(
-        client: Client?, message: String?, mailer: Mailer
+        client: Client?, message: String?,
+        mailer: Mailer
 ){
-    TODO()
+    val email = client?.personalInfo?.email
+    if (email != null && message != null) mailer.sendMessage(email, message)
+
 }
 
 class Client (val personalInfo: PersonalInfo?)
 class PersonalInfo (val email: String?)
 interface Mailer {
-    fun sendMessage(email: String, message: String)
+    fun sendMessage(email: String,
+                    message: String)
 }
