9d7c9b095a4b9a214465ecd1e33469d5ffc3f6e8
Pablo Munoz <pfariasmunoz@gmail.com>
2017-12-20 17:20:51 -0300

    Add Object expressions

--- lesson1/task11/src/Task.kt
+++ lesson1/task11/src/Task.kt
@@ -1,7 +1,8 @@
 import java.util.*
+import kotlin.Comparator
 
 fun getList(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, object {})
+    Collections.sort(arrayList) { o1, o2 -> o2.minus(o1) }
     return arrayList
 }
