cd62a3e1dbe45c42fce4c4476d7262834d0423e3
Pablo Munoz <pfariasmunoz@gmail.com>
2017-12-20 18:46:27 -0300

    Add operator overloading

--- lesson2/task1/src/Task.kt
+++ lesson2/task1/src/Task.kt
@@ -1,5 +1,11 @@
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-    /* TODO */
+    override fun compareTo(other: MyDate): Int {
+        return when {
+            this.year != other.year -> this.year.minus(other.year)
+            this.month != other.month -> this.month.minus(other.month)
+            else -> this.dayOfMonth.minus(other.dayOfMonth)
+        }
+    }
 }
 
 fun compare(date1: MyDate, date2: MyDate) = date1 < date2
