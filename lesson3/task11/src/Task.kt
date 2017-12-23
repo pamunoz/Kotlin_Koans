b2ea88632a39ee3007140d682d0109ba9313446d
Pablo Munoz <pfariasmunoz@gmail.com>
2017-12-23 16:14:54 -0300

    Add Compound tasks

--- lesson3/task11/src/Task.kt
+++ lesson3/task11/src/Task.kt
@@ -1,13 +1,18 @@
 // Return the most expensive product among all delivered products
 // (use the Order.isDelivered flag)
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
-    TODO()
+    val allOrders = customers.flatMap { orders }
+    val allProducts = allOrders.filter { it.isDelivered }.flatMap { it.products }
+    return allProducts.maxBy { it.price }
 }
 
 // Return how many times the given product was ordered.
 // Note: a customer may order the same product for several times.
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
-    TODO()
+
+    val allOrders = customers.flatMap { it.orders }
+    var allProducts = allOrders.filter { it.products.contains(product) }.map { it.products }
+    return allProducts.size
 }
 
-/* TODO */
+
